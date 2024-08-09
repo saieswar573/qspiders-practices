47b9ac45ce78523a791f9c6a37b8953fa5efe691
saieswar573 <saieswar750@gmail.com>
2024-08-09 21:36:24 +0530

    data types added

---
+++ datatypes.py
@@ -0,0 +1,47 @@
+# integer data types
+a=10
+print(a,type(a))
+
+# float datatype
+b=1.0
+print(b,type(b))
+
+# complex datatype
+c=1+2j
+print(c,type(c))
+
+# string datatype
+name=("eswar")
+print(name,type(name))
+
+# bollean datatype
+val=True
+print(val,type(val))
+
+# comment line 
+
+# this is acomment line that will not excuted 
+'''this is also ae comment line that will not be excuted '''
+
+
+
+
+# output statement
+# example program
+print(10,20,30,40)
+print(10,20,30,40,sep="--")
+print(10,20,30,40,sep="")
+
+
+
+# note
+# def sep value is space
+# example 
+print("iron\nman")
+
+print(10)
+print(10)
+print(10)
+print(10)
+print(10)
+
