604c6fc9f9d326f5afc0093385c9b295a765413c
saieswar573 <saieswar750@gmail.com>
2024-08-12 23:19:51 +0530

    operations

---
+++ operations.py
@@ -0,0 +1,211 @@
+# arthemetic operation
+# they are total 7 type of opertions are they they are +,-,*,/,%,//,**
+a=10
+b=20
+print(a+b)
+# showing the int value in the + opertaor 
+a=10
+b=20.5
+print(a+b)
+# it will dispaly the float value 
+a=10
+b=1+2j
+print(a+b)
+# it will display the complex value 
+a=20.5
+b=1+2j
+print(a+b)
+#  it will display the complex value
+a=1+2j
+b=2+2j
+print(a+b)
+#  it will dispaly the complex value
+a=10
+b="don"
+print(a+b)
+# it will dispaly the type error
+a=20.5
+b="don"
+print(a+b)
+#  it will dispaly the type error
+a=1+2j
+b="don"
+print(a+b)
+# it will dispaly the type error 
+a="don"
+b="don"
+print(a+b)
+#  it will dispaly the output str+str=str
+
+
+
+# (-) operation 
+#  showing the int value in - operation int-int=int
+a=10
+b=20
+print(a-b)
+# showing the float value in - operation int-float=float
+a=10
+b=20.5
+print(a-b)
+# showing the complex value in the - operation int-complex=complex
+a=10
+b=1+2j
+print(a-b)
+# showing the complex value in the -opertaion float-complex=complex
+a=20.5
+b=1+2j
+print(a-b)
+# showing the complex value in the -operation complex-complex=complex
+a=1+2j
+b=1+2j
+print(a-b)
+# int-str= type error any thing with the string it will display the value error
+a=10
+b="don"
+print(a-b)
+# float-str= type error any thing with the string it will display the value error
+a=10.3
+b="eswar"
+print(a-b)
+# complex-str= type error any thing with the string it will dispaly the value error
+a=1+2j
+b="eswar"
+print(a-b)
+# str-str=type error any thing with the string it will display the value error
+a="saieswar"
+b="tharun"
+print(a-b)
+
+
+
+# (*) operation 
+# int*int=int showing the the int value in multipication 
+a=10
+b=20
+print(a*b)
+# int*float=float showing the float value in multipication
+a=20
+b=20.4
+print(a*b)
+# int*complex=complex  showing the complex value in multipication
+a=20
+b=1+2j
+print(a*b)
+# float*complex=complex showing the complex value in multipication
+a=20.3
+b=1+2j
+print(a*b)
+# complex*complex=complex showing the complex value in multipication
+a=1+2j
+b=1+2j
+print(a*b)
+# float*string=typeerror 
+a=10.23
+b="eswar"
+print(a*b)
+# complex*string=type error 
+a=1+2j
+b"eswar"
+print(a*b)
+# string*string=typeerror
+a="don"
+b="don"
+print(a*b)
+# int*str=string
+a="100"
+b="saieswar"
+print(a*b)
+
+
+
+# / divison operation
+# int/int=float showing the float value in division
+a=10
+b=20
+print(a/b)
+# int/float=float showing the float value in division
+a=10
+b=20.2
+print(a/b)
+# int/complex=complex showing the complex value in division if any operation with using the complex it will show the complex value 
+a=10
+b=1+2j
+print(a/b)
+# int/str=typeerror showing the type error in division if any operation with using the str it will show the type error
+a=10
+b="don"
+print(a/b)
+
+
+
+
+# (%) modules 
+# int%int=int showing the int value in modules
+a=10
+b=20
+print(a%b)
+# int%float=float showing the float value in modules
+a=10
+b=20.0
+print(a%b)
+# if any operends is comple or string it will show the type error
+a=10
+b=1+3j
+print(a%b)
+# if any operends is comple or string it will show the type error
+a=10
+b="don"
+print(a%b)
+
+
+
+# floor division//
+# int//int=int in the floor division (roundoff ) to the lesser VALUE 
+a=10
+b=20
+print(a//b)
+# int//float=float it wil diplay the float value 
+a=10
+b=10.3
+print(a//b)
+# if any operand is complex or string it will display the type error
+a=10
+b=1+2j
+print(a//b)
+# # if any operand is complex or string it will display the type error
+a=10
+b="don"
+print(a//b)
+
+
+
+
+
+# expornent or power values **
+# int**int=int it will display the ouput 
+a=10
+b=20
+print(a**b)
+# int**float=float it will display the output
+a=10
+b=23
+print(a**b)
+# int**complex=complex it will display the complex value 
+a=10
+b=1+2j
+print(a**b)
+# float**complex=complex it will display the complex value 
+a=10
+b=1+2j
+print(a**b)
+# complex**complex=complex it will display the complex value
+a=1+2j
+b=2+3j
+print(a**b)
+# if any operenters in str it will display the type error
+a=10
+b="don"
+print(a**b)
+
+
